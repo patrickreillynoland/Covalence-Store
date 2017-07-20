dcd5e55a1297c26a281528ebd4e30ee7b149c7da
patrickreillynoland <patrickreillynoland@gmail.com>
2017-07-20 10:35:53 -0400

    Basket adjustments

--- client/factories.js
+++ client/factories.js
@@ -13,104 +13,25 @@ angular.module('CovalenceStore.factories', ['ngRoute', 'ngResource'])
     return $resource('/api/checkout/:id');
 }])
 .factory('CartItem', ['$rootScope', '$log', function ($rootScope, $log) {
+    var items = [];
+    var basket = {};
+    
+    basket.addItem = function(item) {
+        items.push(item);
+    };
+    basket.removeItem = function(item) {
+        var index = items.indexOf(item);
+        items.splice(index, 1);
+    };
+    basket.items = function() {
+        return items;
+    };
+    
+    return myBasketService;
 
-        var item = function (id, name, price, quantity, data) {
-            this.setId(id);
-            this.setName(name);
-            this.setPrice(price);
-            this.setQuantity(quantity);
-            this.setData(data);
-        };
-
-
-        item.prototype.setId = function(id){
-            if (id)  this._id = id;
-            else {
-                $log.error('An ID must be provided');
-            }
-        };
-
-        item.prototype.getId = function(){
-            return this._id;
-        };
-
-
-        item.prototype.setName = function(name){
-            if (name)  this._name = name;
-            else {
-                $log.error('A name must be provided');
-            }
-        };
-        item.prototype.getName = function(){
-            return this._name;
-        };
-
-        item.prototype.setPrice = function(price){
-            var priceFloat = parseFloat(price);
-            if (priceFloat) {
-                if (priceFloat <= 0) {
-                    $log.error('A price must be over 0');
-                } else {
-                    this._price = (priceFloat);
-                }
-            } else {
-                $log.error('A price must be provided');
-            }
-        };
-        item.prototype.getPrice = function(){
-            return this._price;
-        };
-
-
-        item.prototype.setQuantity = function(quantity, relative){
-
-
-            var quantityInt = parseInt(quantity);
-            if (quantityInt % 1 === 0){
-                if (relative === true){
-                    this._quantity  += quantityInt;
-                } else {
-                    this._quantity = quantityInt;
-                }
-                if (this._quantity < 1) this._quantity = 1;
-
-            } else {
-                this._quantity = 1;
-                $log.info('Quantity must be an integer and was defaulted to 1');
-            }
-
-
-        };
-
-        item.prototype.getQuantity = function(){
-            return this._quantity;
-        };
-
-        item.prototype.setData = function(data){
-            if (data) this._data = data;
-        };
-
-        item.prototype.getData = function(){
-            if (this._data) return this._data;
-            else $log.info('This item has no data');
-        };
-
-
-        item.prototype.getTotal = function(){
-            return +parseFloat(this.getQuantity() * this.getPrice()).toFixed(2);
-        };
-
-        item.prototype.toObject = function() {
-            return {
-                id: this.getId(),
-                name: this.getName(),
-                price: this.getPrice(),
-                quantity: this.getQuantity(),
-                data: this.getData(),
-                total: this.getTotal()
-            }
-        };
-
-        return item;
-
-    }])
+    function MyCtrl($scope, basket) {
+        $scope.newItem = {};
+        
+        $scope.basket = basket;    
+        }
+}])
