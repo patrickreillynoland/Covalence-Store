a232e7df529f55186d254281c4d98f459e1fe846
patrickreillynoland <patrickreillynoland@gmail.com>
2017-07-23 18:54:20 -0400

    Cart design updated

--- server/controllers/checkout.ctrl.js
+++ server/controllers/checkout.ctrl.js
@@ -8,7 +8,7 @@ router.post('/', function(req, res) {
      let content = `<h3>You have a new message</h3>
                     <h5>From: ${req.body.email}</h5>
                     <p>${req.body.message}</p>`;
-    mailService.sendEmail(customeremail, 'no-reply@covalence.com', 'Order Confirmation', productspurchasedandamount)
+    mailService.sendEmail('test-email@test.com', 'no-reply@covalence.com', 'Order Confirmation', productspurchasedandamount)
     var amount = Number(req.body.amount);
     stripeSvc.charge(req.body.token, amount)
     .then(function(success) {
